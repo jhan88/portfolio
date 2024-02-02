5c8a0ede40011e7d8a50165a3bdfe94bc03ea22b
Jiyeon HAN <47693422+jhan88@users.noreply.github.com>
2024-02-02 18:13:08 +0900

    Fix JS-Home-TypeIt

--- src/type.js
+++ src/type.js
@@ -1,6 +1,22 @@
 'use strict';
 
-new TypeIt('.home__description', { loop: true, speed: 80 })
-  .pause(2000)
-  .delete()
-  .go();
+if (window.innerWidth > 450) {
+  new TypeIt('.home__description', { loop: true, speed: 80 })
+    .pause(2000)
+    .delete()
+    .go();
+} else {
+  document.querySelector('.home__description').innerHTML = '';
+
+  new TypeIt('.home__description', { loop: true, speed: 80 })
+    .type('a web developer')
+    .pause(1000)
+    .delete()
+    .type('a calligrapher')
+    .pause(1000)
+    .delete()
+    .type('a life explorer!')
+    .pause(2000)
+    .delete()
+    .go();
+}
